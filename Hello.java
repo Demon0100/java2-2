444d7ccb2ea03904d889e3a7956e29c0ac0dc920
Demon0100 <202330215@daelim.ac.kr>
2024-03-29 16:48:45 +0900

    4주차 내용정리

--- Hello.java
+++ Hello.java
@@ -1,9 +1,12 @@
-/**
- * Hello
- */
 public class Hello {
-
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        for(int i=1; i<10; i++) { // 각 단의 반복. 1단에서 9단
+            for(int j=1; j<10; J++) { // 각 단의 곱셈
+                System.out.print(i + "x" + j + "=" + i*j); //구구셈 출력
+                System.out.print('\t'); // 하나씩 탭으로 띄기
+            }
+            System.out.println();// 한 단어 끝나면 다음 줄로 커서 이동
+        }
     }
 }
--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.print("월을 입력하시오, : ");
+        int month = scanner.nextInt();
+        if(3 <= month && month <= 5) {
+            System.out.println("봄 입니다.");
+        }
+        else if(6 <= month && month <= 8) {
+            System.out.println("여름 입니다.");
+        }
+        else if(9 <= month && month <= 11) {
+            System.out.println("가을 입니다.");
+        }
+        else {
+            System.out.println("겨울 입니다.");
+        }
+        scanner.close();
     }
+    
 }
